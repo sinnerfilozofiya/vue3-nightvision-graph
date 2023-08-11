e86c4b9d585777c61f3b58accd7dd01c1b3c4fa2
userdarsozbilici <98901933+userdarsozbilici@users.noreply.github.com>
2023-08-11 10:21:52 +0300

    first-commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
-import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import {h, createApp } from 'vue'
+import singleSpaVue from 'single-spa-vue';
 
 createApp(App).mount('#app')
+
+
+const vueLifecycles = singleSpaVue({
+    createApp,
+    appOptions: {
+        render() {
+            return h(App, {
+                props: {
+                    // single-spa props are available on the "this" object. Forward them to your component as needed.
+                    // https://single-spa.js.org/docs/building-applications#lifecyle-props
+                    name: this.name,
+                },
+            });
+        },
+    },
+});
+export const {bootstrap,mount,unmount} = vueLifecycles;
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+
+  build: {
+    target : 'es2022',
+    manifest : true,
+  
+    rollupOptions : {
+      output: {
+        assetFileNames : 'app.[ext]',
+        entryFileNames : 'app.js',
+      },
+      input: 'src/main.js',
+      preserveEntrySignatures: "strict",
+    }
+  }
 })
